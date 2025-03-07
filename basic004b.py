03f5bc7428bebadb356a7e78ea3b2887c5d1fb56
alinedutrasilva <alinedutra@outlook.ie>
2025-03-07 14:59:40 +0000

    Beginners Learning Process.

--- basic004b.py
+++ basic004b.py
@@ -1,17 +1,17 @@
 n = input('Type something: ')
-print('The Primitive Type is: {} ' 
-      'Is it only spaces? {} ' 
-      'Is it a letter? {} ' 
-      'Is it a number? {} ' 
-      'Is it a lower letter/number? {} ' 
-      'Is it an upper letter/number? {} '
-      'Is it a decimal number? {} '
-      'Is it a digit? {} '
-      'Is it ASCII? {} '
-      'Is it an identifier? {} '
-      'Is it printable? {} '
-      'Is it a Capitalized? {} '
-      'Is it alphanumeric? {} '
+print('The Primitive Type is: {} ' "\n"
+      'Is it only spaces? {} ' "\n"
+      'Is it a letter? {} ' "\n"
+      'Is it a number? {} ' "\n"
+      'Is it a lower letter/number? {} ' "\n"
+      'Is it an upper letter/number? {} ' "\n"
+      'Is it a decimal number? {} ' "\n"
+      'Is it a digit? {} ' "\n"
+      'Is it ASCII? {} ' "\n"
+      'Is it an identifier? {} ' "\n"
+      'Is it printable? {} ' "\n"
+      'Is it a Capitalized? {} ' "\n"
+      'Is it alphanumeric? {} ' "\n"
       .format(type(n), n.isspace(), n.isalpha(),
       n.isnumeric(), n.islower(), n.isupper(), n.isdecimal(),
       n.isdigit(), n.isascii(), n.isidentifier(), n.isprintable(),
