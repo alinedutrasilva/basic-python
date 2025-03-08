5d88769607dd1884b84f14a920c4a1ebdb6241ab
alinedutrasilva <alinedutra@outlook.ie>
2025-03-08 15:15:32 +0000

    Beginners Learning Process.

---
+++ basic005g.py
@@ -0,0 +1,6 @@
+n = float(input('How much cash you have in Real (Brazilian currency)? '))
+d = n/3.27
+print('You have then R$', n)
+print('You can exchange for this amount of dollars $', d)
+
+# US$1,00 = R$3,27
