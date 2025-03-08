3f6a5502be89f65cfeba24b26441a9b80c8144d3
alinedutrasilva <alinedutra@outlook.ie>
2025-03-08 14:42:28 +0000

    Beginners Learning Process.

---
+++ basic005e.py
@@ -0,0 +1,4 @@
+n = int(input('What is the number in Meters? '))
+c = n*100
+m = n*1000
+print('The number in Meter is {}' '\n' 'The number in Centimeter is {}' '\n' 'The number in Milimeter is {}'.format(n, c, m))
