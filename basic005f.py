01d504d9423870ee548056cb77e749713313d334
alinedutrasilva <alinedutra@outlook.ie>
2025-03-08 15:02:20 +0000

    Beginners Learning Process.

---
+++ basic005f.py
@@ -0,0 +1,23 @@
+n = int(input('What is the number? '))
+t1 = n*1
+t2 = n*2
+t3 = n*3
+t4 = n*4
+t5 = n*5
+t6 = n*6
+t7 = n*7
+t8 = n*8
+t9 = n*9
+t10 = n*10
+print('The number is: '.format(n))
+print('The Multiplication table is: ')
+print('Number', n, 'x 1 eh {}'.format(t1))
+print('Number', n, 'x 2 eh {}'.format(t2))
+print('Number', n, 'x 3 eh {}'.format(t3))
+print('Number', n, 'x 4 eh {}'.format(t4))
+print('Number', n, 'x 5 eh {}'.format(t5))
+print('Number', n, 'x 6 eh {}'.format(t6))
+print('Number', n, 'x 7 eh {}'.format(t7))
+print('Number', n, 'x 8 eh {}'.format(t8))
+print('Number', n, 'x 9 eh {}'.format(t9))
+print('Number', n, 'x 10 eh {}'.format(t10))
