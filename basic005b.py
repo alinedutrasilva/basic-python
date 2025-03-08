032c7eb1d1f220d3040e092d9b5f126ec59fcffd
alinedutrasilva <alinedutra@outlook.ie>
2025-03-08 16:23:17 +0000

    Beginners Learning Process.

--- basic005b.py
+++ basic005b.py
@@ -2,3 +2,6 @@ n = int(input('What is the number? '))
 a = n-1
 su = n+1
 print('The number is {}' '\n' 'The antecessor is {}' '\n' 'The successor is {} '.format(n, a, su))
+
+# n = int(input('What is the number? '))
+# print('The number is {}' '\n' 'The antecessor is {}' '\n' 'The successor is {} '.format(n, (n-1), (n+1)))
