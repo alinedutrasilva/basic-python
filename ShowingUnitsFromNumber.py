587ff1e5b8d3ccc23f016302c7b21947fcfeec22
alinedutrasilva <alinedutra@outlook.ie>
2025-03-10 16:50:54 +0000

    Beginners Learning Process.

--- ShowingUnitsFromNumber.py
+++ ShowingUnitsFromNumber.py
@@ -2,4 +2,4 @@ n1 = input('What is the number between 0 to 9999?')
 print("""Unit: {}
 Ten: {}
 Hundred: {}
-Thousand: {}""".format(n1[0], n1[1], n1[2], n1[3]))
+Thousand: {}""".format(n1[3], n1[2], n1[1], n1[0]))
