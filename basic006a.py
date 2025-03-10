05ca030e3afa56eeb3ac0b9fe275d8d9f403aadc
alinedutrasilva <alinedutra@outlook.ie>
2025-03-10 10:50:49 +0000

    Beginners Learning Process.

--- basic006a.py
+++ basic006a.py
@@ -1,3 +1,7 @@
 n = float(input('What is the floating number? '))
 r = int(n)
 print('Your floating number is {} But the integer number is {}'.format(n, r))
+
+# Alternative
+# from math import trunc
+# r = trunc(n)
