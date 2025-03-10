c6b7177b0416990263b637a8fe48a962c5cef16f
alinedutrasilva <alinedutra@outlook.ie>
2025-03-10 11:14:25 +0000

    Beginners Learning Process.

--- basic006c.py
+++ basic006c.py
@@ -3,3 +3,6 @@ co = float(input('Type side of right triangle: '))
 ca = float(input('Type the the other side of the triangle: '))
 h = math.hypot(co, ca)
 print('The hypotenuse is {:.3f}'.format(h))
+
+# Alternative
+# h = (co ** 2 + ca ** 2) ** (1/2)
