d3e2dde4879644f2eef34603f1e09fa185dee670
alinedutrasilva <alinedutra@outlook.ie>
2025-03-10 11:47:52 +0000

    Beginners Learning Process.

---
+++ basic006e.py
@@ -0,0 +1,8 @@
+import random
+n1 = str(input('What is the name of the student 1? '))
+n2 = str(input('What is the name of the student 2? '))
+n3 = str(input('What is the name of the student 3? '))
+n4 = str(input('What is the name of the student 4? '))
+list = [n1, n2, n3, n4]
+r = random.choice(list)
+print('The student selected is {}'.format(r))
