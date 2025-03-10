20e40c8c27af56eec158892ab65e88e0db4aabd4
alinedutrasilva <alinedutra@outlook.ie>
2025-03-10 11:38:17 +0000

    Beginners Learning Process.

--- basic006c.py
+++ basic006c.py
@@ -4,5 +4,12 @@ ca = float(input('Type the the other side of the triangle: '))
 h = math.hypot(co, ca)
 print('The hypotenuse is {:.3f}'.format(h))
 
-# Alternative
+# Alternative 1:
 # h = (co ** 2 + ca ** 2) ** (1/2)
+
+# Alternative 2:
+# from math import hypot
+# co = float(input('Type side of right triangle: '))
+# ca = float(input('Type the the other side of the triangle: '))
+# h = hypot(co, ca)
+# print('The hypotenuse is {:.3f}'.format(h))
--- basic006d.py
+++ basic006d.py
@@ -1,6 +1,18 @@
 import math
 a = float(input('What is the angle? '))
-s = math.sin(a)
-c = math.cos(a)
-t = math.tan(a)
+s = math.sin((math.radians(a)))
+c = math.cos((math.radians(a)))
+t = math.tan((math.radians(a)))
 print('The angle {} has the sine {:.3f} o cosine {:.3f} and tangent {:.3f}.'.format(a, s, c, t))
+
+# Alternative 1:
+# import math
+# a = float(input('What is the angle? '))
+# print('The angle {} has the sine {:.3f} o cosine {:.3f} and tangent {:.3f}.'.format(math.sin(math.radians(a), math.sin(math.radians(s), math.sin(math.radians(c), math.sin(math.radians(t)))
+
+# Alternative 2:
+# from math import radians, sin, cos, tan
+# s = sin((radians(a)))
+# c = cos((radians(a)))
+# t = tan((radians(a)))
+# print('The angle {} has the sine {:.3f} o cosine {:.3f} and tangent {:.3f}.'.format(a, s, c, t))
