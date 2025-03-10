0fc69e62f8b2ceb6adad19012b9bca8a8995f3d7
alinedutrasilva <alinedutra@outlook.ie>
2025-03-10 17:03:07 +0000

    Beginners Learning Process.

--- UsingFind.py
+++ UsingFind.py
@@ -1,3 +1,4 @@
 city = input('What is the name of your city?')
-print("""Your city is: {}
-Does it have the word 'Los' within?""".format(city), 'los' in city)
+m = city.upper()
+print("""Your city is: {}.
+Does it have the word 'Los' within?""".format(city), 'LOS' in m)
