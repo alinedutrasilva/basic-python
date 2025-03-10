42943d5a8019923cb26bd436edce86febeef0eeb
alinedutrasilva <alinedutra@outlook.ie>
2025-03-10 17:52:22 +0000

    Beginners Learning Process.

--- ManipulationString_01.py
+++ ManipulationString_01.py
@@ -1,10 +1,7 @@
-name = str(input('What is your full name? '))
+name = str(input('What is your full name?')).strip()
 m = name.upper()
 n = name.lower()
-q = name.split()
-l = len(name)
-pname = len(q[1])
-print("""You Full name with upper letters is: {} 
-You Full name with lower letters is: {}
-Your full name contains {} letter without spaces
-Your first name contains {} letters.""".format(m, n, l, pname))
+print("""Your full name in capslock is: {} 
+Your full name with capslock off is: {}
+Your full name contains {} letters.
+Your first name contains {} letters.""".format(m, n, len(name) - name.count(' '), name.find(' ')))
