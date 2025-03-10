af52b447e6b5b0c06eb1d0f413520d92031e48b3
alinedutrasilva <alinedutra@outlook.ie>
2025-03-10 12:24:40 +0000

    Beginners Learning Process.

--- basic006f.py
+++ basic006f.py
@@ -4,15 +4,15 @@ n2 = str(input('What is the name of the student 2? '))
 n3 = str(input('What is the name of the student 3? '))
 n4 = str(input('What is the name of the student 4? '))
 list = [n1, n2, n3, n4]
-random.choice(list)
-print('The student selected is {}'.format(list))
+random.shuffle(list)
+print('The order of the presentation will be: {}'.format(list))
 
 # Alternative 1:
-# from random import choice
+# from random import shuffle
 # n1 = str(input('What is the name of the student 1? '))
 # n2 = str(input('What is the name of the student 2? '))
 # n3 = str(input('What is the name of the student 3? '))
 # n4 = str(input('What is the name of the student 4? '))
 # list = [n1, n2, n3, n4]
-# choice(lista)
-# print('The student selected is {}'.format(list))
+# shuffle(list)
+#print('The order of the presentation will be: {}'.format(list))
