d1ed58bbecf7405eeb5a1c08df4cfc51fa150546
alinedutrasilva <alinedutra@outlook.ie>
2025-03-11 13:17:54 +0000

    Beginners Learning Process.

--- UsingFind_03.py
+++ UsingFind_03.py
@@ -15,3 +15,12 @@ The last time the letter 'A' was in the position {}.""".format(c, l, r))
 # print("""The letter 'A' shows up {} times in this phrase.
 # The first time the letter 'A' was in the position {}.
 # The last time the letter 'A' was in the position {}.""".format(c, l, r))
+
+# Alternative 2:
+# f = str(input('Write a phrase:')).strip().upper()
+# c = f.count('A')
+# l = f.find('A') <---- So the counting would not mention/count as "posicao 0", which would confuse final user.
+# r = f.rfind('A') <---- So the counting would not mention/count as "posicao 0", which would confuse final user.
+# print("""The letter 'A' shows up {} times in this phrase.
+# The first time the letter 'A' was in the position {}.
+# The last time the letter 'A' was in the position {}.""".format(c, l, r))
